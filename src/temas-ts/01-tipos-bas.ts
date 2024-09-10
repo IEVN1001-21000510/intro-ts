e0993affd938a2e06f3c4ac0e141e99b94b79852
Christian Cano <christian.canoo@icloud.com>
2024-09-09 21:02:20 -0600

    Primer Commit lunes 09/09/2024

---
+++ src/temas-ts/01-tipos-bas.ts
@@ -0,0 +1,24 @@
+//console.log('Hola Mundo')
+
+var nombre= 'Juan'
+let apellido = 'perez'
+const edad = 10
+
+let num1 = 2
+let num2 = 'wewewy'
+let num3 = 3
+
+let suma = num1 + num2 + num3
+console.log(suma)
+
+let correo:string;
+let edad2:number;
+let activo:boolean;
+
+correo = 'chistian@gmail.com'
+edad2 = 22
+activo = true
+let matricula:number|string|boolean
+
+matricula = 12345
+matricula = '23AD45'
